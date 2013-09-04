51216aebbc32c1b7eebe55c7bd12f40986515816
bliti <pablo.rivera.programmer@gmail.com>
2013-09-04 02:41:44 -0400

    index on the way

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_tempate
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
@@ -8,7 +8,7 @@ DEBUG = True
 
 @app.route("/", methods=['GET'])
 def index():
-    return render_template('index.html')
+    return render_template('index.html', css='index', title='Welcome')
 
 
 if __name__ == "__main__":
