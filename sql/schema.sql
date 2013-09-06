d98f05e5499751f703caef6bd68212a9f303a12e
bliti <pablo.rivera.programmer@gmail.com>
2013-09-06 02:22:35 -0400

    updated schema

--- sql/schema.sql
+++ sql/schema.sql
@@ -11,16 +11,34 @@ create table if not exists users (
 );
 
 
-
 create table if not exists receivers (
     id integer primary key autoincrement,
-    user_id varchar(255) not null,
     name varchar(255) not null,
     phone varchar(255) not null,
     receiver_type varchar(255) not null,
     status varchar(255) not null,
 	last_call timestamp,
 	next_call timestamp,
+    user_id integer not null,
     foreign key(user_id) references users(id)
-	
+);
+
+
+create table if not exists calls (
+    id integer primary key autoincrement,
+    receiver varchar(255) not null,
+    dial_datetime timestamp not null,
+    result varchar(255) not null,
+    user_id integer not null,
+    script_id integer not null,
+    foreign key(user_id) references users(id),
+    foreign key(script_id) references users(id)
 ); 
+
+
+create table if not exists scripts (
+    id integer primary key autoincrement,
+    user_id integer not null,
+	script text not null,
+    foreign key(user_id) references users(id)
+);
